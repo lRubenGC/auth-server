07cc9ed7802306a987edcd8bb90a14ac21c6fb7a
Ruben <rgarcia@odec.es>
2023-01-04 12:30:56 +0100

    fix: manejo de el resto de rutas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
@@ -22,6 +23,10 @@ app.use( express.json() );
 // Rutas
 app.use( '/api/auth', require('./routes/auth') );
 
+// Manejar demás rutas
+app.get( '*', (req, res) =>{
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+})
 
 
 // Puerto de escucha del servidor
